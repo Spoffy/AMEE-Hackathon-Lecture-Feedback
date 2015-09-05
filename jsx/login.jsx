97de123f00f6ef08dc2087683bc1ebd48cf8968a
Spoffy <spoffeh@gmail.com>
2015-09-05 18:24:34 +0100

    Change to course screen from Login

--- jsx/login.jsx
+++ jsx/login.jsx
@@ -1,14 +1,61 @@
-var MainBody = React.createClass({
+var List = React.createClass({
   render: function() {
     return (
-      <form className="pure-form pure-form-stacked">
+      <div
+        className="pure-menu pure-menu-scrollable">
+        <ul className="pure-menu-list">
+          <li className="pure-menu-item">
+            <a href="#" className="pure-menu-link">
+              Course
+            </a>
+          </li>
+          <li className="pure-menu-item">
+            <a href="#" className="pure-menu-link">
+              Course
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+});
+
+var Login = React.createClass({
+  render: function() {
+    return (
+     <form className="pure-form pure-form-stacked">
         <fieldset>
           <label for="username">Username</label>
           <input id="username" type="email" placeholder="Email"/>
-          <button id="submit" className="pure-button pure-button-primary">Sign in</button>
+          <button id="submit" 
+            onClick={this.props.onSuccess}
+            className="pure-button pure-button-primary">
+            Sign in
+          </button>
         </fieldset>
       </form>
     );
+  }
+});
+
+var MainBody = React.createClass({
+  getInitialState: function() {
+    return {
+      current_screen: 0
+    }
+  },
+
+  changeToCourseList: function() {
+    this.setState({current_screen: 1});
+  },
+
+  render: function() {
+    switch(this.state.current_screen) {
+      case 0:
+        return <Login onSuccess={this.changeToCourseList}/>
+      case 1:
+        return <List />;
+    }
   }
 });
 
