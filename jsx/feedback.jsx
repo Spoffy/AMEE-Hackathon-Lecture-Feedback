63222b844d4655f2382e109feffcf43880f7e484
Spoffy <spoffeh@gmail.com>
2015-09-06 01:10:17 +0100

    Buttons resize correctly.

--- jsx/feedback.jsx
+++ jsx/feedback.jsx
@@ -1,10 +1,40 @@
 var FeedbackPanel = React.createClass({
+  componentDidMount: function() {
+    //Set the buttons to the correct sizes.
+    var upvoteButton = $("#upvote");
+    var downvoteButton = $("#downvote");
+    var lastSize = 0;
+    this.resizeTimer = setInterval(function() {
+      var jWindow = $(window);
+      var size = 0;
+      if(jWindow.height() < jWindow.width() * 2) {
+        size = jWindow.height()/2;
+      } else {
+        size = jWindow.width();
+      }
+
+      if(lastSize != size) {
+        upvoteButton.css("height", size);
+        upvoteButton.css("width", size);
+        downvoteButton.css("height", size);
+        downvoteButton.css("width", size);
+        lastSize = size;
+      }
+    }, 1000);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.resizeTimer);
+  },
+
   render: function() {
     return (
-      <div id="vote_container">
-        <img id="upvote" src="/static/assets/images/upvote.svg"/>
-        <img id="downvote" src="/static/assets/images/downvote.svg"/>
-      </div>
+
+<div id="vote_container">
+  <img id="upvote" className="center-block pure-img" src="/static/assets/images/positive_arrow.svg"/>
+  <img id="downvote" className="center-block pure-img" src="/static/assets/images/negative_arrow.svg"/>
+</div>
+
     );
   }
 });
